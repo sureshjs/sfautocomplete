5868323ea27d65f909221945b7dde16734c8a557
Suresh Soundararajan <suresh.soundararajan@USCHSOUNSUALX6.local>
2012-10-24 10:56:06 -0500

    first commit, not working

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="jqueryui/css/custom-theme/jquery-ui-1.9.0.custom.css">
+		<link href='http://fonts.googleapis.com/css?family=Muli' rel='stylesheet' type='text/css'>
+		<script type="text/javascript" src="jqueryui/js/jquery-1.8.2.js"></script>
+		<script type="text/javascript" src="jqueryui/js/jquery-ui-1.9.0.custom.js"></script>
+		<style type="text/css">
+
+		</style>
+
+		<script>
+		$(document).ready(function(){
+			$('#search').autocomplete({
+				source: 'search.php',
+				minLength: 2,
+				select: function(event, ui) {
+				}
+			});
+		});
+    </script>
+	</head>
+	<body>
+		<div class="ui-widget">
+			<label for="search">Search</label>
+			<input type="text" id="search" name="search" />
+		</div>
+		<div id="test"></div>
+	</body>
+</html>
---
+++ search.php
@@ -0,0 +1,26 @@
+<?php
+	$con = mysql_connect('localhost','root','root');
+	if (!$con) {
+		'dead in the water' . mysql_error();
+	}
+
+	mysql_select_db('symfony',$con);
+
+	$autocomplete_value = mysql_real_escape_string($_GET["term"]);
+
+	$sql = "SELECT name FROM Artist WHERE name LIKE '%$autocomplete_value%' UNION
+			SELECT name FROM Event WHERE name LIKE '%$autocomplete_value%'";
+
+//	$sql = "SELECT name FROM ( SELECT name as name2 FROM Event UNION ALL SELECT lastname, firstname as lastname, firstname FROM User 
+//			UNION ALL SELECT name AS name FROM Artist) AS name WHERE name LIKE '%$autocomplete_value%'";
+
+	$query = mysql_query($sql);
+	echo $sql;  
+
+	$results = array();
+	while ($row = mysql_fetch_array($query, MYSQL_ASSOC)) {
+		array_push($results, array('name' => $row['name']) );
+	}
+
+	echo json_encode($results);
+?>
